7783497b8640fd6fce40078965e76c91bfde444d
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2020-03-26 13:05:26 +0530

    Update TestHelloWorld.java

--- src/test/java/com/sanjib/TestHelloWorld.java
+++ src/test/java/com/sanjib/TestHelloWorld.java
@@ -8,26 +8,8 @@ import org.junit.Test;
 
 public class TestHelloWorld {
 
-   private HelloWorld h;
-	
-   @Before
-   public void setUp() throws Exception 
-   {
-      h = new HelloWorld();
-   }
-
    @Test
-   public void testHelloEmpty() 
-   {
-      assertEquals(h.getName(),"");
-      assertEquals(h.getMessage(),"Hello!");
-   }
-	
-   @Test
-   public void testHelloWorld() 
-   {
-      h.setName("World");
-      assertEquals(h.getName(),"World");
-      assertEquals(h.getMessage(),"Hello World!");
-   }
+    public void sayHello() {
+        new HelloWorld().sayHello();
+    }
 }
